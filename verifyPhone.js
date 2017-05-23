127ce46580a87f250f602ddbe7f5c6f8710fc388
Marius <mhognogi@users.noreply.github.com>
2017-05-23 12:06:48 +0300

    Create verifyPhone.js

---
+++ verifyPhone.js
@@ -0,0 +1,12 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script type="text/javascript" >
+var q = '0123456789';
+var reg = new RegExp('^[0-9]+$');
+if(reg.test(q) && q.length){
+    alert()
+} else {
+    alert('The phone need 10 digits.');
+}
+
+</script>
